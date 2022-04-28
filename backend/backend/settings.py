347a2af2fbe3a3a80430e9d5ee06fdd8a580fd63
Romar Desabille <romardesabille1@gmail.com>
2022-04-28 21:04:19 +0800

    Update

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -102,7 +102,6 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
-'''
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
@@ -120,6 +119,7 @@ DATABASES = {
         'POST': '5432',
     }
 }
+'''
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
